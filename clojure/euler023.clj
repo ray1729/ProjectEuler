3c96922c28f555ffc07ec8f9f5f8e89b6d9565ef
Ray Miller <ray@1729.org.uk>
2009-09-23 22:55:22 +0100

    Really add solution to problem 23

--- clojure/euler023.clj
+++ clojure/euler023.clj
@@ -1,8 +1,22 @@
+(ns ray1729.projecteuler.euler023)
 
-(ns ray1729.projecteuler.euler023
-  ;(:require )
-  ;(:use )
-  ;(:import )
-  )
+(defn proper-divisors
+  ([n]
+    (filter #(zero? (rem n %)) (range 1 (inc (int (/ n 2)))))))
 
+(defn sum-proper-divisors
+  ([n]
+    (reduce + (proper-divisors n))))
 
+(defn abundant? [n] (> (sum-proper-divisors n) n))
+
+(def abundant? (memoize abundant?))
+
+;(defn sums-of-pairs [coll]
+;  (if (empty? coll) '()
+;    (lazy-seq (concat (map #(+ (first coll) %) coll) (sums-of-pairs (rest coll))))))
+
+(defn sum-abundant-pair? [n]
+  (and (>= n 24) (some #(and (abundant? %) (abundant? (- n %))) (range 12 n))))
+
+(reduce + (filter #(not (sum-abundant-pair? %)) (range 1 28123)))
