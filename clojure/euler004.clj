44ee32874a84cfb54a8f6a8e953fdd514ed1ba42
Ray Miller <ray@1729.org.uk>
2009-10-20 21:41:32 +0100

    Some improvements to style and efficiency.
    Use Clojure str and seq to make palindrome? more efficient; reduce rather than apply max, also
    for efficiency.

--- clojure/euler004.clj
+++ clojure/euler004.clj
@@ -1,11 +1,10 @@
 
 (ns ray1729.projecteuler.euler004)
 
-(defn palindrome?
-  ([s] (let [chars (re-seq #"." (str s))]
-         (= chars (reverse chars)))))
+(defn palindrome? [s]
+  (let [s (str s)]
+    (= (seq s) (reverse s))))
 
-(defn euler004
-  ([]
-    (apply max
-      (filter palindrome? (for [a (range 100 1000) b (range a 1000)] (* a b))))))
+(defn euler004 []
+  (reduce max
+    (filter palindrome? (for [a (range 100 1000) b (range a 1000)] (* a b)))))
